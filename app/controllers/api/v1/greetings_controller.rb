fffcc56770813d6fea477f3a24be63b55f0abed5
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-17 09:18:06 +0100

    Add ReadMe file

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,2 +1,11 @@
-class Api::V1::GreetingsController < ApplicationController
+# Controller for handling greetings in the API version 1
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        greetings = Greeting.order('Random()').first
+        render json: greetings
+      end
+    end
+  end
 end
