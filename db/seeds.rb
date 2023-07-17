288149e4f892bc5aa8c89c1e00d018e5821601e9
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-17 09:00:17 +0100

    Migrate file and add data to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+  'Hello World!',
+  'Hello Amaka!',
+  'Hello Jeff',
+  'Lord of the rings',
+  'You look beautiful'
+]
+
+greetings.each do |message|
+  Greeting.create(message:)
+end
