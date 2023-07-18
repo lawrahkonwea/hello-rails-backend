8a68b4f3a58331029d2de13f2915fcc0151455b1
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-18 09:43:05 +0100

    Create routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,26 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './components/Greeting';
+
+import store from './redux/configureStore';
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route element={<Greeting/>} path="/" />
+    </Routes>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+);
